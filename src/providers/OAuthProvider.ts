cf36f97e2b0a3392daa3b2ac24ac71f0a7bb8727
natanaelsc <natanael96@live.com>
2023-03-01 14:58:49 -0300

    feat(oauth): create method to exchange authorization code for tokens

--- src/providers/OAuthProvider.ts
+++ src/providers/OAuthProvider.ts
@@ -14,4 +14,34 @@ export class OAuthProvider {
     console.debug(`Redirect URI: ${redirectURI}`);
     return redirectURI;
   }
+
+  async getToken(code: string): Promise<OAuthProviderResponse> {
+    const response: OAuthProviderResponse = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      body: new URLSearchParams({
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: env.spotify.redirect_uri,
+        client_id: env.spotify.client_id,
+        client_secret: env.spotify.client_secret,
+      }),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+      .then(async res => await res.json())
+      .catch(err => {
+        console.error(err);
+      });
+    const token: OAuthProviderResponse = {
+      access_token: response.access_token,
+      refresh_token: response.refresh_token,
+    };
+    return token;
+  }
+}
+
+interface OAuthProviderResponse {
+  access_token: string;
+  refresh_token: string;
 }
