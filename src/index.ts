82a86e676a8e83e252c497a10276a94ec3d19d84
natanaelsc <natanael96@live.com>
2023-03-01 12:02:24 -0300

    refactor: use nodejs default http library

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import http from 'http';
 import app from './app';
 
 const PORT = 5001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.set('port', PORT);
+
+const server = http.createServer(app);
+
+server
+  .listen(PORT, () => {
+    console.debug(`Server is running on port ${PORT}`);
+  })
+  .on('error', err => {
+    console.error(err);
+  });
