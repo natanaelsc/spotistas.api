c351ded8392404dd1863c097e302ca10574cd6a8
natanaelsc <natanael96@live.com>
2023-03-01 13:32:26 -0300

    feat(oauth): remove promise void return

--- src/controllers/OAuthController.ts
+++ src/controllers/OAuthController.ts
@@ -2,7 +2,7 @@ import { type Request, type Response } from 'express';
 import { OAuthProvider } from '../providers/OAuthProvider';
 
 export class OAuthController {
-  async auth(req: Request, res: Response): Promise<void> {
+  auth(_req: Request, res: Response): void {
     const state = 'state';
     const oauthProvider = new OAuthProvider();
     const redirectURI = oauthProvider.getRedirectUri(state as string);
