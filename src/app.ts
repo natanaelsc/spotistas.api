58b241eef886d8880af3c9965dc9382c74cb1841
natanaelsc <natanael96@live.com>
2023-03-01 11:56:08 -0300

    refactor: add cors options

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
-import express from 'express';
 import cors from 'cors';
+import express from 'express';
 
 const app = express();
 
+const corsOptions = {
+  origin: undefined,
+  methods: ['GET', 'POST'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
-app.use(cors());
 
 export default app;
