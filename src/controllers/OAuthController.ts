103dd18a94cfa4f916294c0ba204dd61bac4a256
natanaelsc <natanael96@live.com>
2023-03-06 10:58:56 -0300

    feat(oauth): add token to cookie and redirect to client uri

--- src/controllers/OAuthController.ts
+++ src/controllers/OAuthController.ts
@@ -2,9 +2,10 @@ import { type Request, type Response } from 'express';
 import config from '../config/env';
 import { generateRandomString } from '../utils/crypto';
 import { type OAuthProvider } from './../providers/OAuthProvider';
+import logger from '../config/logger';
 
 class OAuthController {
-  private readonly stateKey = 'spotify_auth_state';
+  private readonly stateKey = 'auth_state';
   private readonly clientUri = config.spotify.client_uri;
 
   constructor(private readonly oauthProvider: OAuthProvider) {}
@@ -19,16 +20,18 @@ class OAuthController {
 
   callback = async (req: Request, res: Response): Promise<Response> => {
     const { code, state } = req.query;
-    const storedState = req.cookies !== null ? req.cookies[this.stateKey] : null;
+    const storedState = req.cookies != null ? req.cookies[this.stateKey] : null;
+    if (state !== storedState) return res.status(400).send({ error: 'state mismatch' });
+    res.clearCookie(this.stateKey);
     try {
-      if (state !== storedState) return res.status(400).send({ error: 'state mismatch' });
-      res.clearCookie(this.stateKey);
-      const data = await this.oauthProvider.getToken(code as string);
-      res.redirect(this.clientUri);
-      return res.status(200).send(data);
-    } catch (error) {
-      return res.status(500).send({ message: 'Something went wrong' }).end();
+      const { access_token: token } = await this.oauthProvider.getToken(code as string);
+      res.cookie('token', token);
+      res.status(302).redirect(this.clientUri);
+    } catch (err) {
+      logger.error(err);
+      return res.status(400).send({ message: 'invalid code' }).end();
     }
+    return res.end();
   };
 }
 
