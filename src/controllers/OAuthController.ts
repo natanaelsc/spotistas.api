3e96af856205af9a1c8ffa850c006e0f5b034373
natanaelsc <natanael96@live.com>
2023-03-07 12:13:51 -0300

    feat(oauth): add cookie options with httpOnly

--- src/controllers/OAuthController.ts
+++ src/controllers/OAuthController.ts
@@ -1,7 +1,8 @@
 import { type Request, type Response } from 'express';
 import config from '../config/env';
 import logger from '../config/logger';
-import { generateRandomString } from '../utils/crypto';
+import { generateRandomString } from '../helpers/crypto';
+import cookie from '../middlewares/cookie';
 import { type OAuthProvider } from './../providers/OAuthProvider';
 
 export class OAuthController {
@@ -11,8 +12,8 @@ export class OAuthController {
 
   auth = (_req: Request, res: Response): Response => {
     const state = generateRandomString(16, 'base64url');
+    res.cookie(this.stateKey, state, cookie.options);
     const redirectUri = this.oauthProvider.getRedirectUri(state);
-    res.cookie(this.stateKey, state);
     res.status(302).redirect(redirectUri);
     return res.end();
   };
@@ -25,11 +26,11 @@ export class OAuthController {
     try {
       const { client_uri: clientUri } = config;
       const { access_token: token } = await this.oauthProvider.getToken(code as string);
-      res.cookie('token', token);
+      res.cookie('token', token, cookie.options);
       res.status(302).redirect(clientUri);
     } catch (err) {
       logger.error(err);
-      return res.status(400).send({ message: 'invalid code' }).end();
+      return res.status(403).send({ message: 'bad oauth request' });
     }
     return res.end();
   };
--- src/utils/crypto.ts
+++
@@ -1,5 +0,0 @@
-import { randomBytes } from 'node:crypto';
-
-export const generateRandomString = (length = 20, encode: BufferEncoding = 'base64'): string => {
-  return randomBytes(length).toString(encode);
-};
