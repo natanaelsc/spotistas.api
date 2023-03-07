88e0c7eeef6f12874a2556788c9a18a29a46d043
natanaelsc <natanael96@live.com>
2023-03-07 12:12:55 -0300

    feat(auth): create user router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import oauthRouter from './oauth';
+import userRouter from './user';
 
 const routes = Router();
 
+routes.get('/', (_req, res) => res.send({ message: 'Hello World!' }));
 routes.use('/oauth', oauthRouter);
+routes.use('/me', userRouter);
 
 export default routes;
