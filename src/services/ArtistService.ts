65e97cb5d9222f69461dab9c22baa1f5d435ea8f
natanaelsc <natanael96@live.com>
2023-03-14 16:31:55 -0300

    feat: add artist top tracks

--- src/services/ArtistService.ts
+++ src/services/ArtistService.ts
@@ -1,10 +1,30 @@
-import { type Artist } from '../interfaces/models/Artist';
+import { type ArtistMonth } from '../interfaces/models/Artist';
+import { type Track } from '../interfaces/models/Track';
+import { type ArtistProvider } from '../providers/ArtistProvider';
+import { type ClientAuthProvider } from '../providers/ClientAuthProvider';
 import { type ArtistRepository } from '../repositories/ArtistRepository';
 
 export class ArtistService {
-  constructor(private readonly artistRepository: ArtistRepository) {}
-  getArtistMonth = async (): Promise<Artist> => {
+  constructor(
+    private readonly clientAuthProvider: ClientAuthProvider,
+    private readonly artistProvider: ArtistProvider,
+    private readonly artistRepository: ArtistRepository
+  ) {}
+
+  getArtistMonth = async (): Promise<ArtistMonth> => {
     const artist = await this.artistRepository.getArtistMonth();
-    return artist;
+    const token = await this.clientAuthProvider.getAccessToken();
+    const topTracks = await this.artistProvider.getArtistTopTracks(token, artist.id);
+    const tracks: Track[] = topTracks.map(track => ({
+      id: track.id,
+      name: track.name,
+      image: track.album.images[0].url,
+      preview_url: track.preview_url,
+      external_url: track.external_urls.spotify,
+    }));
+    return {
+      ...artist,
+      tracks,
+    };
   };
 }
