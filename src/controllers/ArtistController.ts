8cd583932302160fa5bf726aefab34775cec8a2e
natanaelsc <natanael96@live.com>
2023-03-14 16:22:37 -0300

    refactor: add space between methods

--- src/controllers/ArtistController.ts
+++ src/controllers/ArtistController.ts
@@ -3,6 +3,7 @@ import { type ArtistService } from '../services/ArtistService';
 
 export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
+
   getArtistMonth = async (_req: Request, res: Response): Promise<Response> => {
     const artist = await this.artistService.getArtistMonth();
     return res.status(200).json(artist);
