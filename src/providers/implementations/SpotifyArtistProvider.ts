f2526d321e37c17718da190489625d9c2292251d
natanaelsc <natanael96@live.com>
2023-03-20 19:56:34 -0300

    style: change fetch api for axios to get artist top tracks

--- src/providers/implementations/SpotifyArtistProvider.ts
+++ src/providers/implementations/SpotifyArtistProvider.ts
@@ -1,14 +1,26 @@
+import axios from 'axios';
+import logger from '../../config/logger';
 import { type ArtistProvider } from '../ArtistProvider';
 import { type TrackProviderDto } from '../TrackProvider';
 
 export class SpotifyArtistProvider implements ArtistProvider {
+  // getArtistTopTracks = async (token: string, id: string): Promise<TrackProviderDto[]> => {
+  //   const response: Response = await fetch(`https://api.spotify.com/v1/artists/${id}/top-tracks?country=BR`, {
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   });
+  //   const data = await response.json();
+  //   return data.tracks as TrackProviderDto[];
+  // };
+
   getArtistTopTracks = async (token: string, id: string): Promise<TrackProviderDto[]> => {
-    const response: Response = await fetch(`https://api.spotify.com/v1/artists/${id}/top-tracks?country=BR`, {
+    const { data } = await axios.get(`https://api.spotify.com/v1/artists/${id}/top-tracks?country=BR`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
-    const data = await response.json();
+    logger.debug(data);
     return data.tracks as TrackProviderDto[];
   };
 }
