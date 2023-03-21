89dacfb2b34d292bc2d6429754b20735132244dc
natanaelsc <natanael96@live.com>
2023-03-21 18:18:20 -0300

    feat(server): create HttpStatus to return the status code of request

--- src/presentation/http/HttpResponse.ts
+++ src/presentation/http/HttpResponse.ts
@@ -3,6 +3,22 @@ export interface HttpResponse {
   clearCookie: (key: string) => void;
 }
 
+export enum HttpStatus {
+  OK = 200,
+  CREATED = 201,
+  ACCEPTED = 202,
+  NO_CONTENT = 204,
+  FOUND = 302,
+  NOT_MODIFIED = 304,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  CONFLICT = 409,
+  TOO_MANY_REQUESTS = 429,
+  SERVER_ERROR = 500,
+}
+
 export interface CookieOptions {
   name?: string | undefined;
   secret?: string | undefined;
