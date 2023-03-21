c070a7cb2e14364fa26ec38ea813c09f22edf69d
natanaelsc <natanael96@live.com>
2023-03-21 16:19:50 -0300

    perf(mapper): add track mapper to track service

--- src/services/TrackService.ts
+++ src/services/TrackService.ts
@@ -1,7 +1,8 @@
+import { type MapperProvider } from '../interfaces/mappers/MapperProvider';
 import { type ArtistProvider } from '../providers/ArtistProvider';
 import { type ClientAuthProvider } from '../providers/ClientAuthProvider';
 import { type PlaylistProvider } from '../providers/PlaylistProvider';
-import { type TrackProvider } from '../providers/TrackProvider';
+import { type TrackProvider, type TrackProviderDto } from '../providers/TrackProvider';
 import db from './../database/db.json';
 import { type MusicOfDay, type Track } from './../interfaces/models/Track';
 
@@ -12,67 +13,21 @@ export class TrackService {
     private readonly clientAuthProvider: ClientAuthProvider,
     private readonly trackProvider: TrackProvider,
     private readonly artistProvider: ArtistProvider,
-    private readonly playlistProvider: PlaylistProvider
+    private readonly playlistProvider: PlaylistProvider,
+    private readonly mapperProvider: MapperProvider<Track, TrackProviderDto>
   ) {}
 
   getTrack = async (id: string): Promise<Track> => {
     const token = await this.clientAuthProvider.getAccessToken();
-    const track = await this.trackProvider.getTrack(token, id);
-    return {
-      id: track.id,
-      name: track.name,
-      image: track.album.images[0].url,
-      preview_url: track.preview_url,
-      external_url: track.external_urls.spotify,
-      duration_ms: track.duration_ms,
-      popularity: track.popularity,
-      artists: track.artists.map(artist => ({
-        id: artist.id,
-        name: artist.name,
-        genres: artist.genres,
-        popularity: artist.popularity,
-        external_url: artist.external_urls.spotify,
-      })),
-      album: {
-        id: track.album.id,
-        name: track.album.name,
-        image: track.album.images[0].url,
-        release_date: track.album.release_date,
-        external_url: track.album.external_urls.spotify,
-      },
-    };
+    const trackProviderDto = await this.trackProvider.getTrack(token, id);
+    return this.mapperProvider.toModel(trackProviderDto);
   };
 
   getTop = async (top = 'brazil', limit = 5): Promise<Track[]> => {
     const token = await this.clientAuthProvider.getAccessToken();
-    const playlist = await this.playlistProvider.getPlaylist(token, this.db.playlists[0].id);
-    if (playlist.tracks == null) return [];
-    const tracks: Track[] = playlist.tracks
-      .map(track => ({
-        id: track.id,
-        name: track.name,
-        image: track.album.images[0].url,
-        preview_url: track.preview_url,
-        external_url: track.external_urls.spotify,
-        duration_ms: track.duration_ms,
-        popularity: track.popularity,
-        artists: track.artists.map(artist => ({
-          id: artist.id,
-          name: artist.name,
-          genres: artist.genres,
-          popularity: artist.popularity,
-          external_url: artist.external_urls.spotify,
-        })),
-        album: {
-          id: track.album.id,
-          name: track.album.name,
-          image: track.album.images[0].url,
-          release_date: track.album.release_date,
-          external_url: track.album.external_urls.spotify,
-        },
-      }))
-      .slice(0, limit);
-    return tracks;
+    const playlistProviderDto = await this.playlistProvider.getPlaylist(token, this.db.playlists[0].id);
+    if (playlistProviderDto.tracks == null) return [];
+    return this.mapperProvider.toModelList(playlistProviderDto.tracks.slice(0, limit));
   };
 
   getTrackOfTheDay = async (): Promise<MusicOfDay> => {
