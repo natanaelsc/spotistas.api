364ca705bf41303250c40560e994ed67e0295a95
natanaelsc <natanael96@live.com>
2023-03-21 10:20:46 -0300

    style(morgan): create new morgan middleware

--- src/Server.ts
+++ src/Server.ts
@@ -3,9 +3,8 @@ import express, { type Express } from 'express';
 import http from 'http';
 import { Env } from './config/Env';
 import logger from './config/logger';
-import { Cors } from './middlewares/Cors';
+import { Cors, Morgan } from './middlewares';
 import cookieMiddleware from './middlewares/cookie';
-import morganMiddleware from './middlewares/morgan';
 import routes from './routes';
 
 export default class Server {
@@ -29,7 +28,7 @@ export default class Server {
   }
 
   private readonly configure = (): void => {
-    this._app.use(morganMiddleware);
+    this._app.use(Morgan.middleware);
     this._app.use(Cors.middleware);
     this._app.use(cookieMiddleware.parser);
     this._app.use(cookieMiddleware.session);
---
+++ src/middlewares/Morgan.ts
@@ -0,0 +1,26 @@
+import morgan, { type StreamOptions } from 'morgan';
+import { Env } from '../config/Env';
+import logger from '../config/logger';
+
+export class Morgan {
+  private static readonly _format = Env.get('MORGAN', 'dev');
+
+  private static readonly stream: StreamOptions = {
+    write: (message: string) => {
+      logger.http(message);
+    },
+  };
+
+  private static readonly skip = (): boolean => {
+    return Env.isProduction();
+  };
+
+  public static readonly middleware = morgan(this._format, {
+    stream: this.stream,
+    skip: this.skip,
+  });
+
+  static {
+    logger.debug('Morgan:', this.skip() ? 'DISABLED' : 'ENABLED');
+  }
+}
