512e26bba0b357cadb0ac0ba2f83475e7a7b2d1c
natanaelsc <natanael96@live.com>
2023-03-21 14:42:31 -0300

    feat(track): add getTrackOfTheDay method

--- src/controllers/TrackController.ts
+++ src/controllers/TrackController.ts
@@ -1,4 +1,5 @@
 import { type Request, type Response } from 'express';
+import logger from '../config/logger';
 import { type TrackService } from '../services/TrackService';
 
 export class TrackController {
@@ -11,5 +12,15 @@ export class TrackController {
     if (Number.isNaN(limitToNumber)) return res.status(400).send({ message: 'limit must be a number' });
     const tracks = await this.trackService.getTop(topToString, limitToNumber);
     return res.status(200).json(tracks);
+  };
+
+  getTrackOfTheDay = async (_req: Request, res: Response): Promise<Response> => {
+    try {
+      const track = await this.trackService.getTrackOfTheDay();
+      return res.status(200).json(track);
+    } catch (error) {
+      logger.error(error);
+      return res.end();
+    }
   };
 }
