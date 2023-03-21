0031218de8327152cbb81ebdc3bb92a598baff54
natanaelsc <natanael96@live.com>
2023-03-21 16:21:19 -0300

    perf(mapper): add new instance of track mapper to service

--- src/routes/tracks.ts
+++ src/routes/tracks.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { TrackController } from '../controllers/TrackController';
+import { TrackMapperProvider } from '../mappers/providers/TrackMapperProvider';
 import { SpotifyArtistProvider } from '../providers/implementations/SpotifyArtistProvider';
 import { SpotifyClientAuthProvider } from '../providers/implementations/SpotifyClientAuthProvider';
 import { SpotifyPlaylistProvider } from '../providers/implementations/SpotifyPlaylistProvider';
@@ -12,7 +13,8 @@ const trackService = new TrackService(
   new SpotifyClientAuthProvider(),
   new SpotifyTrackProvider(),
   new SpotifyArtistProvider(),
-  new SpotifyPlaylistProvider()
+  new SpotifyPlaylistProvider(),
+  new TrackMapperProvider()
 );
 const trackController = new TrackController(trackService);
 
