46a7052c58aaae74b28ab00581e4d0e8713a1f3f
natanaelsc <natanael96@live.com>
2023-03-21 14:43:18 -0300

    feat(track): add track of the day route

--- src/routes/tracks.ts
+++ src/routes/tracks.ts
@@ -1,15 +1,23 @@
 import { Router } from 'express';
 import { TrackController } from '../controllers/TrackController';
+import { SpotifyArtistProvider } from '../providers/implementations/SpotifyArtistProvider';
 import { SpotifyClientAuthProvider } from '../providers/implementations/SpotifyClientAuthProvider';
 import { SpotifyPlaylistProvider } from '../providers/implementations/SpotifyPlaylistProvider';
+import { SpotifyTrackProvider } from '../providers/implementations/SpotifyTrackProvider';
 import { TrackService } from '../services/TrackService';
 
 const router = Router();
 
-const trackService = new TrackService(new SpotifyClientAuthProvider(), new SpotifyPlaylistProvider());
+const trackService = new TrackService(
+  new SpotifyClientAuthProvider(),
+  new SpotifyTrackProvider(),
+  new SpotifyArtistProvider(),
+  new SpotifyPlaylistProvider()
+);
 const trackController = new TrackController(trackService);
 
 router.get('/', trackController.getTrack);
+router.get('/day', trackController.getTrackOfTheDay);
 router.get('/topBrazil', trackController.getTrack);
 
 export default router as Router;
