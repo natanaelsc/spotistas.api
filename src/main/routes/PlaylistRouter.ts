06b8f49b8d7d9f688700fc4cda981192042fbc7b
natanaelsc <natanael96@live.com>
2023-03-21 22:18:20 -0300

    perf(cache): add cache to playlist route

--- src/main/routes/PlaylistRouter.ts
+++ src/main/routes/PlaylistRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { PlaylistControllerFactory } from '../../factories/controllers';
+import { Cache } from '../middlewares';
 
 export class PlaylistRouter {
   private static readonly _prefix = '/playlists';
   private static readonly _factory = PlaylistControllerFactory.create();
   private static readonly _router = Router();
+  private static readonly _cache = Cache.middleware;
 
   private static readonly routes = (): Router => {
     const router = Router();
@@ -13,6 +15,6 @@ export class PlaylistRouter {
   };
 
   static create = (): Router => {
-    return this._router.use(this._prefix, this.routes());
+    return this._router.use(this._prefix, this._cache, this.routes());
   };
 }
