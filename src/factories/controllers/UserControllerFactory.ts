bf2c52517332c8103a2f98dd304aa8fc8057042c
natanaelsc <natanael96@live.com>
2023-03-22 15:03:31 -0300

    feat: add TrackMapperProvider to UserControllerFactory

--- src/factories/controllers/UserControllerFactory.ts
+++ src/factories/controllers/UserControllerFactory.ts
@@ -1,11 +1,12 @@
 import { SpotifyUserProvider } from '../../infra/providers';
+import { TrackMapperProvider } from '../../mappers/providers';
 import { UserMapperProvider } from '../../mappers/providers/UserMapperProvider';
 import { UserController } from '../../presentation/controllers';
 import { UserService } from '../../services/UserService';
 
 export class UserControllerFactory {
   static create = (): UserController => {
-    const userService = new UserService(new SpotifyUserProvider(), new UserMapperProvider());
+    const userService = new UserService(new SpotifyUserProvider(), new UserMapperProvider(), new TrackMapperProvider());
     return new UserController(userService);
   };
 }
