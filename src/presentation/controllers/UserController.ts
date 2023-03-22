cd9aa287275e785136a1258dac0ead29b9301180
natanaelsc <natanael96@live.com>
2023-03-22 15:02:28 -0300

    feat: create getUserTopTracks method with cache of the results

--- src/presentation/controllers/UserController.ts
+++ src/presentation/controllers/UserController.ts
@@ -7,13 +7,31 @@ import { HttpStatus } from '../http';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  handle = async (req: Request, res: Response): Promise<Response> => {
+  getUser = async (req: Request, res: Response): Promise<Response> => {
     try {
       const token = Cookie.get(req, 'token');
       const user = await this.userService.getUser(token);
       if (user == null) return res.status(HttpStatus.UNAUTHORIZED).send({ error: 'unauthorized' });
       Cache.get(req.originalUrl, user);
       return res.status(HttpStatus.OK).json(user);
+    } catch (error) {
+      const { status, message } = ErrorHandler.catch(error);
+      return res.status(status).send({ error: message });
+    }
+  };
+
+  getUserTopTracks = async (req: Request, res: Response): Promise<Response> => {
+    const { time, limit } = req.query;
+    const limitToNumber = limit == null ? 20 : Number(limit);
+    if (Number.isNaN(limitToNumber))
+      return res.status(HttpStatus.BAD_REQUEST).send({ error: 'limit must be a number' });
+    try {
+      const token = Cookie.get(req, 'token');
+      const tracks = await this.userService.getUserTopTracks(token, String(time), limitToNumber);
+      if (tracks == null) return res.status(HttpStatus.UNAUTHORIZED).send({ error: 'unauthorized' });
+      if (tracks.length === 0) return res.status(HttpStatus.OK).send({ message: 'no results found' });
+      Cache.get(req.originalUrl, tracks);
+      return res.status(HttpStatus.OK).json(tracks);
     } catch (error) {
       const { status, message } = ErrorHandler.catch(error);
       return res.status(status).send({ error: message });
