faa9de8695bab0c9b39ccaa0caf76f3de0f2970b
natanaelsc <natanael96@live.com>
2023-03-21 22:19:18 -0300

    perf(error): add error handler to track and playlist service

--- src/services/PlaylistService.ts
+++ src/services/PlaylistService.ts
@@ -1,6 +1,7 @@
 import { type SpotifyClientAuthProvider } from '../infra/providers';
 import { type Playlist } from '../interfaces/models/Playlist';
 import { type PlaylistProvider } from '../interfaces/providers';
+import { ErrorHandler } from '../presentation/errors';
 
 export class PlaylistService {
   constructor(
@@ -9,17 +10,22 @@ export class PlaylistService {
   ) {}
 
   getOurPlaylists = async (limit = 5): Promise<Playlist[]> => {
-    const token = await this.clientAuthProvider.getAccessToken();
-    const playlists = await this.playlistProvider.getOurPlaylists(token);
-    const ourPlaylists: Playlist[] = playlists
-      .map(playlist => ({
-        id: playlist.id,
-        name: playlist.name,
-        image: playlist.images[0].url,
-        description: playlist.description,
-        external_url: playlist.external_urls.spotify,
-      }))
-      .slice(0, limit);
-    return ourPlaylists;
+    try {
+      const token = await this.clientAuthProvider.getAccessToken();
+      const playlists = await this.playlistProvider.getOurPlaylists(token);
+      return playlists
+        .map(playlist => ({
+          id: playlist.id,
+          name: playlist.name,
+          image: playlist.images[0].url,
+          description: playlist.description,
+          followers: playlist.followers?.total,
+          external_url: playlist.external_urls.spotify,
+        }))
+        .slice(0, limit);
+    } catch (error) {
+      ErrorHandler.catch(error);
+      return [];
+    }
   };
 }
