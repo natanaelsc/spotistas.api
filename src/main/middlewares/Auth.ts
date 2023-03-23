f455e71bcc69c0294ef134af7f4df47761185650
natanaelsc <natanael96@live.com>
2023-03-23 11:42:30 -0300

    refactor: add user provider propertie

--- src/main/middlewares/Auth.ts
+++ src/main/middlewares/Auth.ts
@@ -5,12 +5,13 @@ import { HttpStatus } from '../../presentation/http';
 import { Cookie } from './Cookie';
 
 export class Auth {
+  private static _userProvider;
+
   public static readonly user = async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
     try {
       const token = Cookie.get(req, 'token');
       if (token == null) return res.status(HttpStatus.FORBIDDEN).send({ error: 'missing token' });
-      const userProvider = new SpotifyUserProvider();
-      const user = await userProvider.getUser(token);
+      const user = await this._userProvider.getUser(token);
       if (user.id == null) return res.status(HttpStatus.UNAUTHORIZED).send({ error: 'invalid token' });
       next();
       return res.status(HttpStatus.OK);
@@ -23,4 +24,8 @@ export class Auth {
   public static readonly middleware = {
     user: this.user,
   };
+
+  static {
+    this._userProvider = new SpotifyUserProvider();
+  }
 }
